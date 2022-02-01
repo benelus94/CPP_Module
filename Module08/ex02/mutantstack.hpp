138ceebf0f72ac6c8df1a24bfda57993a21d16b2
김용욱 <gim-yong-ug@gim-yong-ug-ui-MacBookAir.local>
2022-02-01 12:58:29 +0900

    mutantstack rbegin rend added

--- Module08/ex02/mutantstack.hpp
+++ Module08/ex02/mutantstack.hpp
@@ -6,7 +6,7 @@
 /*   By: yongwkim <yongwkim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/13 14:38:30 by yongwkim          #+#    #+#             */
-/*   Updated: 2022/01/17 16:29:11 by yongwkim         ###   ########.fr       */
+/*   Updated: 2022/02/01 12:57:42 by yongwkim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,8 +28,18 @@ class MutantStack : public std::stack<T>
 		~MutantStack() {};
 
 		typedef typename std::stack<T>::container_type::iterator iterator;
+		typedef typename std::stack<T>::container_type::reverse_iterator reverse_iterator;
+		typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+		typedef typename std::stack<T>::container_type::const_reverse_iterator const_reverse_iterator;
+
 		iterator begin(void) {return (this->c.begin());};
 		iterator end(void) {return (this->c.end());};
+		const_iterator begin(void) const {return (this->c.begin());};
+		const_iterator end(void) const {return (this->c.end());};
+		reverse_iterator rbegin(void) {return (this->c.rbegin());};
+		reverse_iterator rend(void) {return (this->c.rend());};
+		const_reverse_iterator rbegin(void) const {return (this->c.rbegin());};
+		const_reverse_iterator rend(void) const {return (this->c.rend());};
 };
 
 #endif
